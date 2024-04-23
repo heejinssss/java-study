c6d08611b83dad8d1a8bd8d9689cc70d187caa2c
heejinssss <heejin.dev33@gmail.com>
2024-04-23 15:02:22 +0900

    Feat: Practice01-Prob3

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -3,13 +3,22 @@ package prob3;
 import java.util.Scanner;
 
 public class Prob3 {
-	
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
 
-		/* 코드 작성 */
-		
+		System.out.print("숫자를 입력하세요. : ");
+		int number = scanner.nextInt();
+
+		int temp = number;
+		int total = 0;
+
+		while (temp > 0) {
+			total += temp;
+			temp -= 2;
+		}
+
+		System.out.println(total);
+
 		scanner.close();
 	}
 }
