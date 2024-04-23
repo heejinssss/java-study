2fe3727db97dc95cfab15b1d701b18312f60dbb3
heejinssss <heejin.dev33@gmail.com>
2024-04-23 16:52:24 +0900

    Feat: Practice02-Prob4

--- practice02/src/main/java/prob04/Prob04.java
+++ practice02/src/main/java/prob04/Prob04.java
@@ -2,20 +2,28 @@ package prob04;
 
 public class Prob04 {
 	public static void main(String[] args) {
-		char[] c1 = reverse( "Hello World" );
-		printCharArray( c1 );
-		
-		char[] c2 = reverse( "Java Programming!" );
-		printCharArray( c2 );
-	}
-	
-	public static char[] reverse(String str) {
-		/* 코드를 완성합니다 */
-		return null;
+		char[] c1 = reverse("Hello World");
+		printCharArray(c1);
+
+		char[] c2 = reverse("Java Programming!");
+		printCharArray(c2);
 	}
 
-	public static void printCharArray(char[] array){
+	public static char[] reverse(String str) {
+		char[] arr = str.toCharArray();
+		int size = arr.length;
+		char[] reverseArr = new char[size];
+
+		int idx = 0;
+		for (int i = size - 1; i >= 0; i--) {
+			reverseArr[idx] = arr[i];
+			idx++;
+		}
+		return reverseArr;
+	}
+
+	public static void printCharArray(char[] array) {
 		/* 코드를 완성합니다 */
-		System.out.println( array );
+		System.out.println(array);
 	}
 }
