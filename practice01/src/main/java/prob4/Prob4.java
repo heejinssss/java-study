b8a1e83bd8f65e7bd46067ce5e8c47bbb30cae8a
heejinssss <heejin.dev33@gmail.com>
2024-04-23 15:13:09 +0900

    Feat: Practice01-Prob4

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -10,8 +10,19 @@ public class Prob4 {
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
 
-		
+		String str = text;
+		String[] arr = str.split("");
+
+		for (int i = 0; i < text.length(); i++) {
+			int idx = 0;
+			while (idx <= i) {
+				System.out.print(arr[idx]);
+				idx += 1;
+			}
+
+			System.out.println();
+		}
+
 		scanner.close();
 	}
-
 }
