b0745d2fa3a28d9489677ad33e8c3290b84d109e
heejinssss <heejin.dev33@gmail.com>
2024-04-23 15:32:18 +0900

    Feat: Practice01-Prob5

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -1,7 +1,23 @@
 package prob5;
 
 public class Prob5 {
-
 	public static void main(String[] args) {
+
+		for (int i = 3; i <= 99; i++) {
+			String str = Integer.toString(i);
+			String[] arr = str.split("");
+			boolean flag = false;
+
+			for (int j = 0; j < arr.length; j++) {
+				int temp = Integer.parseInt(arr[j]);
+				if (temp % 3 == 0 & temp != 0) {
+					flag = true;
+				}
+			}
+
+			if (flag) {
+				System.out.println(i + " 짝");
+			}
+		}
 	}
 }
