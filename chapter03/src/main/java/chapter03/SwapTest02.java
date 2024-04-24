003ce1cfe6ad4e3260e7298f63f547d5ffff9a0f
heejinssss <heejin.dev33@gmail.com>
2024-04-24 14:29:00 +0900

    Feat: Chapter03-Swap

--- chapter03/src/main/java/chapter03/SwapTest02.java
+++ chapter03/src/main/java/chapter03/SwapTest02.java
@@ -13,7 +13,6 @@ public class SwapTest02 {
 	}
 
 	// 외부 함수에서 변수 값을 바꿀 수 없음 (swap 함수의 결과로 i와 j가 교차되지 않음)
-	// Call by Value
 	private static void swap(int n1, int n2) {
 		int temp = n1;
 		n1 = n2;
