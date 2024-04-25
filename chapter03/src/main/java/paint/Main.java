990d7147a37fd2c5fafeba67af2199abb8cfe716
heejinssss <heejin.dev33@gmail.com>
2024-04-25 13:28:31 +0900

    Refactor: Chapter03-Extends

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -2,15 +2,22 @@ package paint;
 
 public class Main {
 	public static void main(String[] args) {
-		Point point = new Point(10, 20);
+		Point point1 = new Point(10, 20);
 //		point.setX(10);
 //		point.setY(20);
-		drawPoint(point);
+		drawPoint(point1);
 //		point.disappear();
-		point.show(false);
+		point1.show(false);
+
+		ColorPoint point2 = new ColorPoint(100, 200, "red");
+		drawPoint(point2);
 	}
 
 	private static void drawPoint(Point point) {
 		point.show();
 	}
+
+//	private static void drawColorPoint(ColorPoint colorPoint) {
+//		colorPoint.show();
+//	}
 }
