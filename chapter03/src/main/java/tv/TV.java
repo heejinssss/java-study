574db249e2482fa0c7cd7d335d65d7df05ea9066
heejinssss <heejin.dev33@gmail.com>
2024-04-25 16:59:47 +0900

    Style: Chapter03-WatchTV

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -21,19 +21,22 @@ public class TV {
 
 	public void isPower(boolean on) {
 		if (on) {
-			System.out.println("TV 전원 ON");
+			System.out.println("ON");
 		}
 		this.power = on;
 	}
 
+	// 1~255
 	public void channel(int channel) {
 		if (channel <= 255 & channel >= 1) {
 			this.channel = channel;
 		} else {
-			System.out.println("없는 채널입니다.");
+			System.out.println("없는 채널입니다. 현재 채널을 유지합니다.");
 		}
 	}
 
+	// 1일 때 !up이면 255 return
+	// 255일 때 up이면 0 return
 	public void channel(boolean up) {
 		if (channel < 255 & channel > 1) {
 			if (up) {
@@ -41,8 +44,10 @@ public class TV {
 			} else {
 				channel--;
 			}
-		} else if ((channel == 255 & up) | (channel == 1 & !up)) {
-			System.out.println("없는 채널입니다.");
+		} else if (channel == 255 & up) {
+			channel = 0;
+		} else if (channel == 1 & !up) {
+			channel = 255;
 		}
 	}
 
@@ -50,7 +55,7 @@ public class TV {
 		if (volume <= 100 & volume >= 0) {
 			this.volume = volume;
 		} else {
-			System.out.println("잘못된 입력입니다.");
+			System.out.println("올바른 볼륨 값을 입력하세요.");
 		}
 	}
 
@@ -70,9 +75,19 @@ public class TV {
 
 	public void status() {
 		if (power) {
-			System.out.println("현재 볼륨은 " + getVolume() + "이고, 현재 채널은 " + getChannel() + "번입니다.");
+			System.out.println("## " + "Volume= " + getVolume() + ", Channel=" + getChannel() + " ##");
 		} else {
-			System.out.println("TV 전원 OFF");
+			System.out.println("OFF");
 		}
+	}
+
+	public void hi() {
+		System.out.println(" _______  _______  ___      _______  __   __  ___   _______  ___   _______  __    _ \r\n"
+						 + "|       ||       ||   |    |       ||  | |  ||   | |       ||   | |       ||  |  | |\r\n"
+						 + "|_     _||    ___||   |    |    ___||  |_|  ||   | |  _____||   | |   _   ||   |_| |\r\n"
+						 + "  |   |  |   |___ |   |    |   |___ |       ||   | | |_____ |   | |  | |  ||       |\r\n"
+						 + "  |   |  |    ___||   |___ |    ___||       ||   | |_____  ||   | |  |_|  ||  _    |\r\n"
+						 + "  |   |  |   |___ |       ||   |___  |     | |   |  _____| ||   | |       || | |   |\r\n"
+						 + "  |___|  |_______||_______||_______|  |___|  |___| |_______||___| |_______||_|  |__|");
 	}
 }
