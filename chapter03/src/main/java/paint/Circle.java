c21e37e80157a3ec49ba373ecb78f3f14fa0b621
heejinssss <heejin.dev33@gmail.com>
2024-04-26 09:09:46 +0900

    Feat: Chapter03-PainTest

---
+++ chapter03/src/main/java/paint/Circle.java
@@ -0,0 +1,10 @@
+package paint;
+
+public class Circle extends Shape {
+	public int radius;
+
+	@Override
+	public void draw() {
+		System.out.println("원을 그렸습니다.");
+	}
+}
--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -5,19 +5,65 @@ public class Main {
 		Point point1 = new Point(10, 20);
 //		point.setX(10);
 //		point.setY(20);
-		drawPoint(point1);
+//		drawPoint(point1);
+		draw(point1);
+
 //		point.disappear();
 		point1.show(false);
 
 		ColorPoint point2 = new ColorPoint(100, 200, "red");
-		drawPoint(point2);
+//		drawColorPoint(point2);
+		draw(point2);
+
+//		drawTriangle(new Triangle());
+//		drawRectangle(new Rectangle());
+		draw(new Triangle());
+		draw(new Rectangle());
+		draw(new Circle());
+		draw(new GraphicText("Hello World"));
+
+		// instanceof 연산자
+		Circle c = new Circle();
+		System.out.println(c instanceof Circle);
+		System.out.println(c instanceof Shape);
+		System.out.println(c instanceof Object);
+
+		/**
+		 * 오류: 연산자 우측항이 클래스인 경우
+		 * 레퍼런스하고 있는 class 타입의 hierarchy 상에서 하위/상위만 instanceof 연산자를 사용할 수 있다.
+		 */
+//		System.out.println(c instanceof Point);
+
+		Object o = new Circle();
+		System.out.println(o instanceof String);
+
+		// 연산자 우측항이 인터페이스인 경우
+		// Hierarchy 상관 없이 instanceof 연산자를 사용할 수 있다.
+		System.out.println(c instanceof Drawable);
+		System.out.println(c instanceof Runnable);
 	}
 
-	private static void drawPoint(Point point) {
-		point.show();
+	private static void draw(Drawable drawable) {
+		drawable.draw();
 	}
+
+//	private static void drawPoint(Point point) {
+//		point.show();
+//	}
 
 //	private static void drawColorPoint(ColorPoint colorPoint) {
 //		colorPoint.show();
+//	}
+
+//	private static void drawShape(Shape shape) {
+//		shape.draw();
+//	}
+
+//	private static void drawTriangle(Triangle triangle) {
+//		triangle.draw();
+//	}
+
+//	private static void drawRectangle(Rectangle rectangle) {
+//		rectangle.draw();
 //	}
 }
