a1f89f31a7b18769e259cf9507bafa5c79cabdfa
heejinssss <heejin.dev33@gmail.com>
2024-04-26 11:25:05 +0900

    Feat: Chatper03-Customize Exception

--- chapter03/src/main/java/exception/MyClass.java
+++ chapter03/src/main/java/exception/MyClass.java
@@ -3,12 +3,16 @@ package exception;
 import java.io.IOException;
 
 public class MyClass {
-	public void danger() throws IOException {
+	public void danger() throws IOException, MyException {
 		System.out.println(1);
 		System.out.println(2);
 
 		// if 조건에 넣지 않으면 이후 코드는 Dead Code
-		if (true) {			
+		if (true) {
+			throw new MyException();
+		}
+
+		if (true) {
 			throw new IOException();
 		}
 
--- chapter03/src/main/java/exception/MyClassTest.java
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -6,7 +6,11 @@ public class MyClassTest {
 	public static void main(String[] args) {
 		try {
 			new MyClass().danger();
+//		} catch (Exception e) { // 모든 에러 한 번에 처리
 		} catch (IOException e) {
+			System.out.println("error: " + e);
+//			e.printStackTrace(); // stack 상태를 구체적으로 출력
+		} catch (MyException e) {
 			System.out.println("error: " + e);
 //			e.printStackTrace(); // stack 상태를 구체적으로 출력
 		}
