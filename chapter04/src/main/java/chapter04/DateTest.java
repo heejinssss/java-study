3216efe0eec0ab41504a1f309b4b8570ec61cf03
heejinssss <heejin.dev33@gmail.com>
2024-04-29 14:30:17 +0900

    Feat: Chapter04-Util(Date)

---
+++ chapter04/src/main/java/chapter04/DateTest.java
@@ -0,0 +1,49 @@
+package chapter04;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTest {
+	public static void main(String[] args) {
+		Date now = new Date();
+		System.out.println(now);
+
+		printDate01(now);
+		printDate02(now);
+	}
+
+	private static void printDate01(Date now) {
+		// 2024-04-29 13:10:20
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String date = sdf.format(now);
+		System.out.println(date);
+	}
+
+	private static void printDate02(Date now) {
+		/* Deprecated */
+		// 년도(+1900)
+		int year = now.getYear();
+
+		// 월(0 ~ 11, +1)
+		int month = now.getMonth();
+
+		// 일
+		int date = now.getDate();
+
+		// 시
+		int hour = now.getHours();
+
+		// 분
+		int minute = now.getMinutes();
+
+		// 초
+		int second = now.getSeconds();
+
+		System.out.println((year + 1900) + "-"
+						  + (month + 1) + "-"
+                          + date + "-"
+                          + hour + ":"
+                          + minute + ":"
+                          + second);
+	}
+}
