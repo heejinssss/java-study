4c88cbeffb445afe64239736cabbc96fe10144c7
heejinssss <heejin.dev33@gmail.com>
2024-04-29 09:07:31 +0900

    Feat: Practice04-Prob1

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,19 @@
+package prob01;
+
+public class Printer {
+	public void println(int num) {
+		System.out.println(num);
+	}
+
+	public void println(boolean TF) {
+		System.out.println(TF);
+	}
+
+	public void println(double num) {
+		System.out.println(num);
+	}
+
+	public void println(String name) {
+		System.out.println(name);
+	}
+}
