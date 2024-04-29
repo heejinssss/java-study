8c0b390416948c7d09a166cfdb58997f8218abba
heejinssss <heejin.dev33@gmail.com>
2024-04-29 10:39:36 +0900

    Style: Chapter04-Wrapper Class

--- chapter04/src/main/java/chapter04/WrapperClassTest01.java
+++ chapter04/src/main/java/chapter04/WrapperClassTest01.java
@@ -8,14 +8,14 @@ public class WrapperClassTest01 {
 		// Deprecated
 		// 왜? 직접 생성할 경우 Heap 상에 객체가 존재하게 된다.
 		// Literal을 사용하면 JVM 안의 Constant Pool에서 관리하게 된다.
-		
+
 		// Auto Boxing
 		Integer i1 = 10;
 		Integer i2 = 20;
-		
+
 		System.out.println(i1 != i2);
 		System.out.println(i1.equals(i2));
-		
+
 		// Auto Unboxing
 		int m = i1 + 10; // (1)
 //		int m = i1.intValue() + 10; // (1)과 동일한 코드 
