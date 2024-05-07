389876ba8886603d3cb90fa242b63a094490069d
heejinssss <heejin.dev33@gmail.com>
2024-05-07 10:29:31 +0900

    Feat: Design Pattern Practice-Observer

---
+++ designpattern-practice/src/main/java/behavioral/observer/Client.java
@@ -0,0 +1,28 @@
+package behavioral.observer;
+
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) {
+		Subject<Integer> intSubject = new Subject<>();
+
+		intSubject.registerObserver(new Observer<Integer>() {
+			@Override
+			public void update(Integer val) {
+				System.out.println("Observer01: " + val);
+			}
+		});
+
+		intSubject.registerObserver((Integer val) -> {
+			System.out.println("Observer01: " + val);
+		});
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print(">> ");
+			int val = scanner.nextInt();
+			intSubject.changeSubject(val);
+		}
+	}
+}
